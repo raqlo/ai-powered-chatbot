6b4afd3dc03c2c5bde6de1b680bbe03f8b56731e
Raquel Lopez <raquelcle@gmail.com>
2025-10-18 20:58:56 -0400

    Connect the frontend and the backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -11,4 +11,10 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 })
 
+app.get('/api/hello', (req: Request, res: Response) => {
+    res.json({
+        message: 'Hello from the server!'
+    });
+})
+
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}! ${process.env.OPENAI_API_KEY}`));
