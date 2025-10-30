db5c9663b4f2d2d943ee4d16bfe0f5b12425e697
Raquel Lopez <raquelcle@gmail.com>
2025-10-29 22:09:13 -0400

    Optimize review summarization with caching, reusable prompt template, and expiration management via `dayjs`.

--- packages/server/repositories/review.respository.ts
+++ packages/server/repositories/review.respository.ts
@@ -1,13 +1,38 @@
+import dayjs from "dayjs";
 import {PrismaClient, type Review} from "../generated/prisma/client";
 
+const prisma = new PrismaClient();
 export const reviewRepository = {
     async getReviews(productId: number, limit?: number): Promise<Review[]> {
-        const prisma = new PrismaClient();
 
         return prisma.review.findMany({
             where: {productId},
             orderBy: {createdAt: 'desc'},
             take: limit
         });
+    },
+    async storeReviewSummary(productId: number, summary: string) {
+
+        const now = new Date();
+        const expiresAt = dayjs().add(7, 'days').toDate();
+        const data = {
+            content: summary,
+            expiresAt,
+            generatedAt: now,
+            productId
+        }
+
+        return prisma.summary.upsert({
+            where: {productId},
+            create: data,
+            update: data
+        })
+
+    },
+
+    getReviewSummary(productId: number) {
+        return prisma.summary.findUnique({
+            where: {productId}
+        })
     }
 };
--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,26 +1,31 @@
 import {OpenAI} from "openai/client";
 import {reviewRepository} from "../repositories/review.respository";
 import {llmClient} from "../llm/client";
+import template from '../prompts/summarize-reviews.txt'
 
 export const reviewService = {
     async getReviews(productId: number) {
         return reviewRepository.getReviews(productId);
     },
     async summarizeReviews(productId: number): Promise<string> {
+        const existingSummary = await reviewRepository.getReviewSummary(productId);
+        if(existingSummary && existingSummary.expiresAt > new Date()) {
+            return existingSummary.content
+        }
+
         const reviews = await reviewRepository.getReviews(productId, 10);
         const joinedReviews = reviews.map(r => r.content).join('\n\n');
-        const prompt = `Summarize the following customer reviews into a short paragraph highlighting key themes, 
-        both positive and negative:
-        
-        ${joinedReviews}`
+        const prompt = template.replace('{{reviews}}', joinedReviews);
 
-        const response =  await llmClient.generateText({
+        const { text: summary} =  await llmClient.generateText({
             model: 'gpt-4o-mini',
             input: prompt,
             temperature: 0.2,
             maxTokens: 500,
         });
-        return response.text;
+
+        await reviewRepository.storeReviewSummary(productId, summary);
+        return summary;
     }
 };
 
